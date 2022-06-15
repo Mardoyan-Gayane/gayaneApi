161a835190ef9cc59a6a9fe18afbb9eee784868f
Mardoyan-Gayane <79018343+Mardoyan-Gayane@users.noreply.github.com>
2022-06-16 02:05:02 +0700

    Api implemented

---
+++ src/gayane/app/Http/Controllers/OrderController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+    //
+    public function index()
+    {
+        return Order::all();
+    }
+ 
+    public function show($id)
+    {
+        return Order::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        return Order::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $order = Order::findOrFail($id);
+        $order->update($request->all());
+
+        return $article;
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $order = Order::findOrFail($id);
+        $order->delete();
+
+        return 204;
+    }
+}
--- src/gayane/routes/api.php
+++ src/gayane/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models;
+use App\Models\Employee;
+use App\Models\Customer;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,44 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('customers', function(){
+    return Customer::all();
+});
+
+Route::get('customers/{id}', function($id){
+    return Customer::find($id);
+});
+
+Route::post('customers', function(Request $request){
+    return Customer::create($request->all);
+});
+
+Route::delete('customers/{id}',function($id){
+    Customer::find($id)->delete();
+    return 204;
+});
+
+
+Route::get('employees', function(){
+    return Employee::all();
+});
+
+Route::get('employees/{id}', function($id){
+    return Employee::find($id);
+});
+
+Route::post('employees', function(Request $request){
+    return Employee::create($request->all);
+});
+
+Route::delete('employees/{id}',function($id){
+    Employee::find($id)->delete();
+    return 204;
+});
+
+Route::get('orders', 'OrderController@index');
+Route::get('orders/{id}', 'OrderController@show');
+Route::post('orders', 'OrderController@store');
+Route::put('order/{id}', 'OrderController@update');
+Route::delete('order/{id}', 'OrderController@delete');
